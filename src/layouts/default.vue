5e31684560ceb7e71c5e57af25ea09adcfde678a
Pablo Gutiérrez <gutiii1997@gmail.com>
2021-08-04 03:38:07 +0200

    feat: use src directory

---
+++ src/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <span
+        >&copy; {{ new Date().getFullYear() }} Pablo Gutiérrez Gomis - TFG
+        Ingeniería Multimedia</span
+      >
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Historical HTR',
+    }
+  },
+}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-btn color="secondary">Button</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import * as tfjs from '@tensorflow/tfjs'
+
+export default Vue.extend({
+  created() {
+    console.log(tfjs)
+  },
+})
+</script>
